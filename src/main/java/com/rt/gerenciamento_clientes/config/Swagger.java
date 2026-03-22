e3760ae00b2796bb5a31e8b109fdc9ed6e09d78c
Renan Takushi <takushirenan@gmail.com>
2026-03-22 18:22:13 -0300

    model e config

---
+++ src/main/java/com/rt/gerenciamento_clientes/config/Swagger.java
@@ -0,0 +1,7 @@
+package com.rt.gerenciamento_clientes.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Swagger {
+}
---
+++ src/test/java/com/rt/gerenciamento_clientes/GerenciamentoClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rt.gerenciamento_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
