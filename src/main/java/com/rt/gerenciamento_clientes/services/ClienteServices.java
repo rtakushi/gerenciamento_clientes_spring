2e704b98474e0bd7035fc06c0b3486f8df41095e
Renan Takushi <takushirenan@gmail.com>
2026-03-22 18:43:47 -0300

    sevices criado

---
+++ src/main/java/com/rt/gerenciamento_clientes/services/ClienteServices.java
@@ -0,0 +1,31 @@
+package com.rt.gerenciamento_clientes.services;
+
+import com.rt.gerenciamento_clientes.models.Cliente;
+import com.rt.gerenciamento_clientes.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteServices {
+    @Autowired
+    private ClienteRepository repository;
+
+    public Cliente salvar(Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    public List<Cliente> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+
+    public void deletar(Long id) {
+        repository.deleteById(id);
+    }
+}
